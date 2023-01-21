ce190688e0e2ee824ad52f8dd58ed31668214734
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-21 13:49:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -21,13 +21,13 @@ const App = () => {
       ballPositionClone.left = Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) - 5 + "px";
     }
     else if(event.keyCode === 39){
-      ballPositionClone.left = Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) - 5 + "px";
+      ballPositionClone.left = Number(ballPosition.left.slice(0, ballPosition.left.indexOf("p"))) + 5 + "px";
     }
     else if(event.keyCode === 38){
       ballPositionClone.top = Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) - 5 + "px";
     }
     else if(event.keyCode === 40){
-      ballPositionClone.top = Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) - 5 + "px";
+      ballPositionClone.top = Number(ballPosition.top.slice(0, ballPosition.top.indexOf("p"))) + 5 + "px";
     }
     setBallPosition(ballPositionClone);
   }
